8e0ca0a7e0e62fa513469d6ded6998f8a8c0db74
Bruna <brunaporato@gmail.com>
2023-11-28 19:52:56 -0300

    add stitches

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,27 @@
+import { styled } from "@/styles"
+
+const Button = styled('button', {
+  backgroundColor: '$rocketseat',
+  borderRadius: 4,
+  cursor: 'pointer',
+  border: 0,
+  padding: '4px 8px',
+
+  span: {
+    fontWeight: 'bold',
+  },
+
+  '&:hover': {
+    filter: 'brightness(0.8)',
+    transition: 'all 0.3s'
+  }
+})
+
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <Button>
+      <span>teste</span>
+      Enviar
+    </Button>
   )
 }
