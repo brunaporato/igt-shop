25ae6893f585f156e39e67be97d612db252b0639
Bruna <brunaporato@gmail.com>
2023-12-01 18:14:30 -0300

    fetch user data for success page

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -1,5 +1,6 @@
 import { stripe } from "@/lib/stripe";
-import { NextApiRequest, NextApiResponse } from "next";
+import { GetServerSideProps, NextApiRequest, NextApiResponse } from "next";
+import Stripe from "stripe";
 
 export default async function handler (req: NextApiRequest, res: NextApiResponse) {
   // return res.json({message: 'hello world'}) // código server side
@@ -13,7 +14,7 @@ export default async function handler (req: NextApiRequest, res: NextApiResponse
     return res.status(400).json({ error: 'Price not found' })
   }
 
-  const successUrl = `${process.env.NEXT_URL}/success`
+  const successUrl = `${process.env.NEXT_URL}/success?session_id={CHECKOUT_SESSION_ID}`
   const cancelUrl = `${process.env.NEXT_URL}/`
 
   const checkoutSession = await stripe.checkout.sessions.create({
