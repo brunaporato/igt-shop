279c68cfde1055d952dfdd3e9d2a25fa660cc926
Bruna <brunaporato@gmail.com>
2023-12-05 16:55:27 -0300

    add remove item function & totalprice

--- src/context/CartContext.tsx
+++ src/context/CartContext.tsx
@@ -11,7 +11,9 @@ export interface ProductType {
 
 interface CartContextProps {
   itemsCart: ProductType[]
+  totalPriceFormatted: string
   addItemToCart: (product: ProductType) => void
+  removeItemFromCart: (product: ProductType) => void
 }
 
 interface CartProviderProps {
@@ -30,11 +32,39 @@ export default function CartProvider({ children }: CartProviderProps) {
     setItemsCart(newCart)
   }
 
+  function removeItemFromCart(product: ProductType) {
+    const indexToRemove = itemsCart.findIndex((item) => item.id === product.id)
+
+    if(indexToRemove !== -1) {
+      const newCart = [...itemsCart]
+      newCart.splice(indexToRemove, 1)
+      setItemsCart(newCart)
+    }
+  }
+
+  function convertPriceToNumber(priceString: String) {
+    const numericString = priceString.replace("R$", "").replace(",", ".").trim();
+    return parseFloat(numericString)
+  }
+
+  const numericPrices = itemsCart.map((item) => {
+    return convertPriceToNumber(item.price)
+  })
+
+  const totalPrice = numericPrices.reduce((accumulator, price) => accumulator + price, 0)
+
+  const totalPriceFormatted = new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(totalPrice)
+
   return (
     <CartContext.Provider
       value={{
         itemsCart,
+        totalPriceFormatted,
         addItemToCart,
+        removeItemFromCart,
       }}
     >
       {children}
